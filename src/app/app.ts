e39c00469b267d18ff6641871ae3188995dd7f71
uwie <157711065+louis-uwie@users.noreply.github.com>
2025-07-07 11:22:22 +0800

    Included infinite scroll. Needs fixing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
+import { NgComponentOutlet } from '@angular/common';
+
+// Import your page components
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Career } from './pages/career/career';
+import { Repositories } from './pages/repositories/repositories';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    CommonModule,
+    NgComponentOutlet,
+    InfiniteScrollDirective,
+    Home,
+    About,
+    Career,
+    Repositories,
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 export class App {
-  protected title = 'louis-uwie';
+  loading = false;
+  page = 0;
+
+  availablePages = [Home, About, Career, Repositories];
+  loadedPages: any[] = [];
+
+  constructor() {
+    this.loadNextPage();
+  }
+
+  loadNextPage() {
+    if (this.page < this.availablePages.length) {
+      this.loading = true;
+      setTimeout(() => {
+        this.loadedPages.push(this.availablePages[this.page]);
+        this.page++;
+        this.loading = false;
+      }, 600);
+    }
+  }
+
+  onScroll() {
+    if (!this.loading) {
+      this.loadNextPage();
+    }
+  }
 }
